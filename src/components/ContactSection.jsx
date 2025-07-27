317e0769bd9b3a93ffa6f7b6da958eb7ad1b96b4
NeerajSuriya <neerajsuriyapadmanabhan@gmail.com>
2025-07-27 14:34:20 +0000

    added email

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -59,7 +59,7 @@ export const ContactSection = () => {
                     href="mailto:hello@gmail.com"
                     className="text-muted-foreground hover:text-primary transition-colors"
                   >
-                    hello@gmail.com
+                    neerajsuriyapadmanabhan@gmail.com
                   </a>
                 </div>
               </div>
